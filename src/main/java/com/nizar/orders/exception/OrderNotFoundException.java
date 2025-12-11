fcb11f1644fc907f2e147c1b59d0f32467661198
Nizar <hussainnizar747@yahoo.com>
2025-12-10 21:07:58 -0500

    Enhance: Add exception handling, GlobalExceptionHandler, and OrderNotFoundException

---
+++ src/main/java/com/nizar/orders/exception/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nizar.orders.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String id) {
+        super("Order not found with id: " + id);
+    }
+}
+
